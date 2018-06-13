ad58d4d1970539f75c09f7ab9f6dd2c9cf790469
Michael <michael.robertson1991@gmail.com>
2018-06-13 11:41:01 -0400

    Recreated project using Maven instead of Gradle

--- src/main/java/com/robertsonwebdev/sunshadersrestservice/Application.java
+++ src/main/java/com/robertsonwebdev/sunshadersrestservice/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SunshadersrestserviceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SunshadersrestserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/main/java/com/robertsonwebdev/sunshadersrestservice/ServletInitializer.java
+++ src/main/java/com/robertsonwebdev/sunshadersrestservice/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SunshadersrestserviceApplication.class);
+		return application.sources(Application.class);
 	}
 
 }
