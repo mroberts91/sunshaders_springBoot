dc83133da4c7f0b7eba9eda6671cfaacf17c3047
Michael Robertson <michael.robertson1991@gmail.com>
2018-07-06 12:20:06 -0400

    Testing appointment post request, to add new appointment, db insert not implemented yet

--- src/main/java/com/robertsonwebdev/sunshadersrestservice/controllers/AppointmentController.java
+++ src/main/java/com/robertsonwebdev/sunshadersrestservice/controllers/AppointmentController.java
@@ -2,22 +2,27 @@ package com.robertsonwebdev.sunshadersrestservice.controllers;
 
 import com.robertsonwebdev.domain.Appointment;
 import com.robertsonwebdev.persistence.ApptRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppointmentController {
 
+    @GetMapping("/appointment")
+    public Appointment addNewAppt(){
+        return new Appointment("Please send", "As Post", "Resquest");
+    }
     @PostMapping("/appointment")
     public ResponseEntity addNewAppointment(
         @RequestBody Appointment appointment){
         try {
             ApptRepository repo = new ApptRepository();
-            repo.createNewAppointment(appointment);
-            return ResponseEntity.ok().body(200);
+            System.out.println(appointment.getFirstname() + " " + appointment.getLastname() + " " + appointment.getScheduled());
+            //repo.createNewAppointment(appointment);
+            return ResponseEntity.ok(HttpStatus.OK);
         } catch (Exception ex){
-            return ResponseEntity.unprocessableEntity().body(400);
+            return ResponseEntity.ok(HttpStatus.I_AM_A_TEAPOT);
         }
-
     }
 }
