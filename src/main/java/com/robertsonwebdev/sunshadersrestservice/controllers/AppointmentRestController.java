6e60ee2dfb9b17a938c7dbc41c763af54f9db2ce
Michael Robertson <michael.robertson1991@gmail.com>
2018-07-12 13:36:00 -0400

    Created admin appointments viewing page

--- src/main/java/com/robertsonwebdev/sunshadersrestservice/controllers/AppointmentRestController.java
+++ src/main/java/com/robertsonwebdev/sunshadersrestservice/controllers/AppointmentRestController.java
@@ -45,13 +45,55 @@ public class AppointmentRestController {
     @GetMapping(path="/all")
     public @ResponseBody List<Appointment> getAllAppointments(){
         List<Appointment> appointments = new ArrayList<>();
-        for(Appointment appt : appointmentRepository.findAll()){
+        for (int x=0; x < 10; x++){
+            Appointment appt = new Appointment();
+            appt.setFirstname("Tom");
+            appt.setLastname("Smith");
+            appt.setAppointemntDate("2018-07-12");
+            appt.setAppointmentTime("08:00:00");
             appointments.add(appt);
         }
+        for (int i=0; i < 5; i++){
+            Appointment appt = new Appointment();
+            appt.setFirstname("Mike");
+            appt.setLastname("Rob");
+            appt.setAppointemntDate("2018-08-11");
+            appt.setAppointmentTime("08:00:00");
+            appointments.add(appt);
+        }
+
+        for (int i=0; i < 5; i++){
+            Appointment appt = new Appointment();
+            appt.setFirstname("Ashley");
+            appt.setLastname("Miller");
+            appt.setAppointemntDate("2017-06-15");
+            appt.setAppointmentTime("08:00:00");
+            appointments.add(appt);
+        }
+
+        for (int i=0; i < 5; i++){
+            Appointment appt = new Appointment();
+            appt.setFirstname("Matthew");
+            appt.setLastname("Cole");
+            appt.setAppointemntDate("2018-07-09");
+            appt.setAppointmentTime("08:00:00");
+            appointments.add(appt);
+        }
+        for (int i=0; i < 100; i++){
+            Appointment appt = new Appointment();
+            appt.setFirstname("Test");
+            appt.setLastname("Test");
+            appt.setAppointemntDate("2018-12-11");
+            appt.setAppointmentTime("11:00:00");
+            appointments.add(appt);
+        }
+//        for(Appointment appt : appointmentRepository.findAll()){
+//            appointments.add(appt);
+//        }
         // Sortt the List by date and then by time
         appointments.sort(Comparator.comparing(Appointment::getAppointemntDate)
                 .thenComparing(Appointment::getAppointmentTime));
-        //appointments.sort(Comparator.comparing(Appointment::getAppointmentTime));
+//        //appointments.sort(Comparator.comparing(Appointment::getAppointmentTime));
         return appointments;
 
 
